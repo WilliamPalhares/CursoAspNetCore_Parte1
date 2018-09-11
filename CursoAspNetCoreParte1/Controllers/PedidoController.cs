be1aefbcaa0e90e320a0bff277c77e02690bacb9
williampalhares <willian-rj@hotmail.com>
2018-09-10 23:30:14 -0300

    Comecando Curso

---
+++ CursoAspNetCoreParte1/Controllers/PedidoController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace CursoAspNetCoreParte1.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private IConfiguration _configuration;
+
+        public PedidoController(IConfiguration Configuration)
+        {
+            _configuration = Configuration;
+        }
+
+        public IActionResult Carrossel()
+        {
+            return View();
+        }
+
+        public IActionResult Carrinho()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public IActionResult Resumo()
+        {
+            return View();
+        }
+    }
+}
---
+++ CursoAspNetCoreParte1/Startup.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using CursoAspNetCoreParte1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CursoAspNetCoreParte1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var Connection = Configuration["ConexaoMySql:MySqlConnectionString"];
+
+            services.AddDbContext<ApplicationContext>(op => op.UseMySql(Connection));
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider ser)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Pedido}/{action=Carrossel}/{id?}");
+            });
+
+            ser.GetService<ApplicationContext>().Database.MigrateAsync();
+        }
+    }
+}
